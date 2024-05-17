5bbec795b21da2dd0cfa3616a2c5c739f1270c97
Broscan <Calle992015@gmail.com>
2024-05-17 09:39:14 +0200

    added loop and useEffect

--- src/components/SpellBook.jsx
+++ src/components/SpellBook.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Spell from "./Spell";
 
-function SpellBook() {
-  return <></>;
+function SpellBook(props) {
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    // Hämta spells och sätt spells-variabeln
+    fetch("http://localhost:3000/spells")
+      .then((res) => res.json())
+      .then((data) => setSpells(data));
+  }, []);
+
+  // //Hook för när spells variabeln ändras
+  //   useEffect(() => {
+  //     if (spells.length > 0) console.log("Spells have been set!");
+  //   }, [spells]);
+
+  return (
+    <>
+      {spells.map((s) => (
+        <h1>{s.name}</h1>
+      ))}
+    </>
+  );
 }
 
 export default SpellBook;
